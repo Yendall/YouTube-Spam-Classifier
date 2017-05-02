716073ae13bf0327f1ffb99a7e70c49b90dbb767
Max Yendall <yendallmax@gmail.com>
2017-05-02 21:38:52 +1000

    Naive Bayes and K-NN

--- include/DocumentCollection.py
+++ include/DocumentCollection.py
@@ -1,5 +1,5 @@
 from sklearn.feature_extraction.text import CountVectorizer
-from sklearn.naive_bayes import MultinomialNB
+
 from settings import *
 import pandas as pd
 from pandas import DataFrame
@@ -20,32 +20,6 @@ class DocumentCollection:
     def __init__(self):
         pass
 
-    def extract_data(self, key, dataset):
-        rows = []
-        index = []
-        for key, data in dataset.iteritems():
-            print data
-            rows.append({'content': data['content'], 'class': data['class']})
-            index.append(key)
-
-        data_frame = DataFrame(rows, index=index)
-        return data_frame
-
-    def naive_bayes(self):
-        count_vectorizer = CountVectorizer()
-        counts = count_vectorizer.fit_transform(self.document_map['Psy']['content'].values)
-
-        classifier = MultinomialNB()
-        targets = self.document_map['Psy']['class'].values
-        classifier.fit(counts, targets)
-
-        examples = ["Gaming channels are cool",
-                    "check out my website for cheap deals www.maxyendall.com.au"]
-        example_counts = count_vectorizer.transform(examples)
-        predictions = classifier.predict(example_counts)
-        print predictions
-
-
     def populate_map(self):
         """
         Populate a document map to store the collection of data for referencing
@@ -61,8 +35,6 @@ class DocumentCollection:
                 file_path = DATA_ROOT + "/" + filename
                 file_key = os.path.splitext(filename)[0]
                 self.document_map[file_key] = pd.read_csv(file_path, sep=',', names=header, skiprows=1)
-
-        return self.split_data()
 
     def print_collection(self):
         """
@@ -81,10 +53,10 @@ class DocumentCollection:
         rand_set = random.choice(self.document_map.keys())
         print self.document_map[rand_set]
 
-    def extract_features(self):
+    def extract_features(self, dataset, count_vectorizer):
         """
         Extract features using a count vectorizer to store word frequencies
         :return:
         """
-        print self.data_frame['content'].values
-        return self.count_vectorizer.fit_transform(self.data_frame['content'].values)
+
+        return count_vectorizer.fit_transform(self.document_map[dataset]['content'].values)
--- parser.py
+++ parser.py
@@ -7,6 +7,38 @@ Assignment due 18 May 2017
 """
 
 from include.DocumentCollection import *
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.neighbors import KNeighborsClassifier
+
+def k_nearest_neighbours(collection):
+    dataset = 'Psy'
+    count_vectorizer = CountVectorizer()
+    counts = collection.extract_features(dataset, count_vectorizer)
+
+    classifier = KNeighborsClassifier()
+    targets = collection.document_map['Psy']['class'].values
+    classifier.fit(counts, targets)
+
+    examples = ["www.maxyendall.com", "check out my website for cheap deals www.maxyendall.com.au"]
+    example_counts = count_vectorizer.transform(examples)
+    predictions = classifier.predict(example_counts)
+    print "K-Nearest-Neighbours Predictions: ", predictions
+
+
+
+def naive_bayes(collection):
+    dataset = 'Psy'
+    count_vectorizer = CountVectorizer()
+    counts = collection.extract_features(dataset,count_vectorizer)
+
+    classifier = MultinomialNB()
+    targets = collection.document_map['Psy']['class'].values
+    classifier.fit(counts, targets)
+
+    examples = ["www.maxyendall.com", "check out my website for cheap deals www.maxyendall.com.au"]
+    example_counts = count_vectorizer.transform(examples)
+    predictions = classifier.predict(example_counts)
+    print "Naive Bayes Predictions: ", predictions
 
 
 def parse_data_collection():
@@ -14,9 +46,11 @@ def parse_data_collection():
     # Create new document collection
     collection = DocumentCollection()
     # Populate the document map with data frames and unique key-sets
-    data = collection.populate_map()
+    collection.populate_map()
 
-    print data
+    naive_bayes(collection)
+    k_nearest_neighbours(collection)
+
 
 
 if __name__ == "__main__": parse_data_collection()
